e30e68d1679727f2ea729a50dd591132dbc8395c
Danila <77702061+dkrgn@users.noreply.github.com>
2024-04-07 02:07:02 +0200

    rooms loader added

---
+++ room-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into rooms (id, description, room_number, type, price, capacity)
+values
+    (1, 'description', '1A', 'ORDINARY', 25.0, 2),
+    (2, 'description', '2A', 'ORDINARY', 25.0, 2),
+    (3, 'description', '3A', 'ORDINARY', 25.0, 2),
+    (4, 'description', '4A', 'VIP', 50.0, 4),
+    (5, 'description', '5A', 'VIP', 50.0, 4),
+    (6, 'description', '6A', 'LUXURY', 50.0, 6)
